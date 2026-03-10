6b255e6eba9e7bbdd1d8e1bc3020ca89af5c22c4
kexy123 <kexy12345678901@gmail.com>
2026-03-11 09:58:15 +1300

    Create Value.

---
+++ Calculator/Value/Number.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Calculator.Value
+{
+    [method: SetsRequiredMembers]
+    public struct Number(double value, string assignedVariable = "") : IValue
+    {
+        public required double Value = value;
+        public required string AssignedVariable = assignedVariable;
+
+        public readonly ValueType Type => ValueType.Number;
+        readonly object IValue.Value => Value;
+        readonly string IValue.AssignedVariable => AssignedVariable;
+
+        public readonly override string ToString() => Convert.ToString(Value);
+        //public readonly override bool Equals([NotNullWhen(true)] object? obj) => obj is Number number && number.Value == Value;
+
+        //public static implicit operator decimal(Number number) => number.Value;
+        public static implicit operator double(Number number) => number.Value;
+
+        public static implicit operator Number(sbyte value) => new(value);
+        public static implicit operator Number(byte value) => new(value);
+        public static implicit operator Number(short value) => new(value);
+        public static implicit operator Number(ushort value) => new(value);
+        public static implicit operator Number(int value) => new(value);
+        public static implicit operator Number(nint value) => new(value);
+        public static implicit operator Number(uint value) => new(value);
+        public static implicit operator Number(nuint value) => new(value);
+        public static implicit operator Number(long value) => new(value);
+        public static implicit operator Number(ulong value) => new(value);
+
+        public static implicit operator Number(float value) => new(value);
+        public static implicit operator Number(double value) => new(value);
+        //public static implicit operator Number(decimal value) => new(value);
+    }
+}
---
+++ Calculator/Value/Value.cs
@@ -0,0 +1,19 @@
+﻿namespace Calculator.Value
+{
+    public enum ValueType
+    {
+        Number,
+        Variable,
+        Expression,
+    }
+
+    public interface IValue {
+        public object Value { get; }
+        public string AssignedVariable { get; }
+        public ValueType Type { get; }
+    }
+
+    public static class Value {
+        public static Number ToNumber(IValue obj) => obj is Number number ? number : throw new ArgumentException(obj + " is not a Number.");
+    }
+}
