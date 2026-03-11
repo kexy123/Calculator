372bb842e47b11108f26e5106dfe5ec158871836
kexy123 <kexy12345678901@gmail.com>
2026-03-11 23:08:33 +1300

    Implement calculator contexts.

---
+++ Calculator/Expression/CalculatorContext.cs
@@ -0,0 +1,14 @@
+﻿using Calculator.Value;
+using Calculator.Variable;
+
+namespace Calculator.Expression
+{
+    public struct CalculatorContext : IVariableContext
+    {
+        public Dictionary<string, Function> Functions => Functions;
+        public Dictionary<string, IValue> Variables => Variables;
+
+        public void ImplementFunction(Function function) => Functions.Add(function.Name, function);
+        public void ImplementVariable(string name, IValue value) => Variables.Add(name, value);
+    }
+}
---
+++ Calculator/Variable/VariableContext.cs
@@ -0,0 +1,23 @@
+﻿using Calculator.Value;
+
+namespace Calculator.Variable
+{
+    public interface IVariableContext
+    {
+        public Dictionary<string, Function> Functions { get; }
+        public Dictionary<string, IValue> Variables { get; }
+
+        /// <summary>
+        /// Implements the given function into the context.
+        /// </summary>
+        /// <param name="function">The function to implement.</param>
+        public void ImplementFunction(Function function);
+
+        /// <summary>
+        /// Implements the given variable into the context.
+        /// </summary>
+        /// <param name="name">The name of the variable.</param>
+        /// <param name="value">The variable's value to implement.</param>
+        public void ImplementVariable(string name, IValue value);
+    }
+}
