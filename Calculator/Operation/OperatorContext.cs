ea202e4cddc4074cb41cf7ca556d79eba4623806
kexy123 <kexy12345678901@gmail.com>
2026-03-12 21:29:39 +1300

    Implement and fix contexts for CalculatorContext.

--- Calculator/Operation/OperatorContext.cs
+++ Calculator/Operation/OperatorContext.cs
@@ -1,4 +1,14 @@
 ﻿namespace Calculator.Operation
 {
+    public interface IOperatorContext
+    {
+        public Operator[] Operators { get; }
 
+        /// <summary>
+        /// Determines the operation given a string of combined operators.
+        /// </summary>
+        /// <param name="source">The string to look at.</param>
+        /// <returns>An Operator derived from that string.</returns>
+        public Operator DetermineOperationFromString(string source);
+    }
 }
--- Calculator/Variable/VariableContext.cs
+++ Calculator/Variable/VariableContext.cs
@@ -8,16 +8,16 @@ namespace Calculator.Variable
         public Dictionary<string, IValue> Variables { get; }
 
         /// <summary>
-        /// Implements the given function into the context.
+        /// Assigns the given function into the context.
         /// </summary>
         /// <param name="function">The function to implement.</param>
-        public void ImplementFunction(Function function);
+        public void AssignFunction(Function function);
 
         /// <summary>
-        /// Implements the given variable into the context.
+        /// Assigns the given variable into the context.
         /// </summary>
         /// <param name="name">The name of the variable.</param>
         /// <param name="value">The variable's value to implement.</param>
-        public void ImplementVariable(string name, IValue value);
+        public void AssignVariable(string name, IValue value);
     }
 }
