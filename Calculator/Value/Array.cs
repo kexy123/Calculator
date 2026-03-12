641de66e2644d34d7630b381bb119c081b5a5d64
kexy123 <kexy12345678901@gmail.com>
2026-03-12 23:29:39 +1300

    Create Array struct.

---
+++ Calculator/Value/Array.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Core.Value
+{
+    [method: SetsRequiredMembers]
+    public struct Array(IValue[] values, string assignedVariable = "") : IValue
+    {
+        public required IValue[] Values = values;
+        public required string AssignedVariable = assignedVariable;
+
+        public readonly ValueType Type => ValueType.Number;
+        readonly object IValue.Value => Values;
+        readonly string IValue.AssignedVariable => AssignedVariable;
+
+        /// <summary>
+        /// Gets the value in the array from the key.
+        /// </summary>
+        /// <param name="key">The value assigned to that key.</param>
+        public readonly IValue GetValue(int key) => Values[key];
+
+        public readonly override string ToString() => string.Join(", ", Values);
+
+        [SetsRequiredMembers]
+        public Array(IValue value, string assignedVariable = "") : this([value], assignedVariable) { }
+    }
+}
--- Calculator/Value/Value.cs
+++ Calculator/Value/Value.cs
@@ -4,6 +4,7 @@
     {
         Number,
         Variable,
+        Array,
         Expression,
     }
 
