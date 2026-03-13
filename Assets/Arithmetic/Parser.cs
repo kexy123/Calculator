8846c2d0ddda2e183e5710e9c3458d17310dc82b
kexy123 <kexy12345678901@gmail.com>
2026-03-13 13:51:21 +1300

    Implement parser interface and boilerplate arithmetic parser code.

---
+++ Assets/Arithmetic/Parser.cs
@@ -0,0 +1,21 @@
+﻿using Core.Expression;
+using Core.Expression.Token;
+
+namespace Core.AssetParsers
+{
+    public class Arithmetic : IParser
+    {
+        public readonly List<Token> Output = [];
+        List<Token> IParser.Output => Output;
+
+        public Token[] Tokens = [];
+        Token[] IParser.Tokens => Tokens;
+
+        public void Parse(Token[] tokens)
+        {
+            Output.Clear();
+            Tokens = tokens;
+            // TODO: Create Arithmetic parser here.
+        }
+    }
+}
---
+++ Calculator/Expression/Parser.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Expression
+{
+    public interface IParser
+    {
+        public List<Token.Token> Output { get; }
+        public Token.Token[] Tokens { get; }
+
+        /// <summary>
+        /// Parses the given array of tokens to easily readable formats.
+        /// </summary>
+        /// <param name="tokens">The array of tokens to parse.</param>
+        public void Parse(Token.Token[] tokens);
+    }
+}
