44c789d67503a4094f0df050503b9370de4d7c3a
kexy123 <kexy12345678901@gmail.com>
2026-03-13 20:19:11 +1300

    Implement State struct and make Tokens in IParser custom.

--- Calculator/Expression/Parser/Parser.cs
+++ Calculator/Expression/Parser/Parser.cs
@@ -1,14 +1,13 @@
-﻿namespace Core.Expression
+﻿namespace Core.Expression.Parser
 {
     public interface IParser
     {
         public List<Token.Token> Output { get; }
-        public Token.Token[] Tokens { get; }
 
         /// <summary>
         /// Parses the given array of tokens to easily readable formats.
         /// </summary>
         /// <param name="tokens">The array of tokens to parse.</param>
-        public void Parse(Token.Token[] tokens);
+        public void Parse(in Token.Token[] tokens);
     }
 }
---
+++ Calculator/Expression/Parser/State.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Core.Expression.Parser
+{
+    using StateDictionary = Dictionary<string, object>;
+
+    public readonly struct State(StateDictionary? states = null)
+    {
+        private readonly StateDictionary States = states ?? [];
+
+        /// <summary>
+        /// Sets a state.
+        /// </summary>
+        /// <param name="name">The name of the state.</param>
+        /// <param name="value">The value of the state.</param>
+        public readonly void Set(string name, object value) => States.Add(name, value);
+
+        /// <summary>
+        /// Gets a state.
+        /// </summary>
+        /// <param name="name">The name of the value of the state.</param>
+        /// <returns>The value of the state.</returns>
+        public readonly object Get(string name) => States[name];
+
+        public static implicit operator State(StateDictionary states) => new(states);
+    }
+}
